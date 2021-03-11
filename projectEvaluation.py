a9c2a4ca8511a39ab0eaa624c2110ee0b7856851
alenjames2000 <alenjames@ufl.edu>
2021-03-11 16:02:57 -0500

    Version 1

--- projectEvaluation.py
+++ projectEvaluation.py
@@ -1,29 +1,36 @@
 import json
-#How to load JSONS and access elements
-data = open("tags.json")
-data = json.load(data)
-guess = open("tags1.json")
-guess = json.load(guess)
 
-total = 0
-correct = 0
-bonus = {}
-bonusTotal = {}
-for i in data:
-    total += 1
-    add = data[i]['signTags'] == guess[i]['signTags']
-    correct += add
-    for x in data[i]['difficultyTags']:
-        if(x in bonusTotal):
-            bonusTotal[x] += 1
-            bonus[x] += add
-        else:
-            bonusTotal[x] = 1
-            bonus[x] = add
+def projectEVAL(truth, output):
+    #Get JSON data
+    data = open(truth)
+    data = json.load(data)
+    guess = open(output)
+    guess = json.load(guess)
 
-extra = 0
-extraPerTag = 5/len(bonusTotal)
-for i in bonusTotal:
-    extra += bonus[i]/bonusTotal[i]*extraPerTag
-    
-print(correct/total*100+extra)
+    total = 0
+    correct = 0
+    bonus = {}
+    bonusTotal = {}
+    for i in data:
+        total += 1
+        add = data[i]['signTags'] == guess[i]['signTags']
+        correct += add
+        #For bonus calculation
+        for x in data[i]['difficultyTags']:
+            if(x in bonusTotal):
+                bonusTotal[x] += 1
+                bonus[x] += add
+            else:
+                bonusTotal[x] = 1
+                bonus[x] = add
+    #Calculate Bonus
+    extra = 0
+    extraPerTag = 5/len(bonusTotal)
+    for i in bonusTotal:
+        extra += bonus[i]/bonusTotal[i]*extraPerTag
+        
+    return correct/total*100+extra
+
+if __name__ == '__main__':
+    score = projectEVAL("tags.json", "tags1.json") # replace with filename (groundtruth, prediction)
+    print(score)
