cc4cf2868e588659130c76d50cb51eb7f9f3d707
Bryan Delgado <32143508+Bryan-Delgado@users.noreply.github.com>
2021-03-25 18:01:00 -0400

    Combined Data and Added RF
    Used all big data for testing. Added the RF classifier and confusion matrices for both classifiers.

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from worddetection import wordDetection
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn import neighbors
+from sklearn.metrics import confusion_matrix
 
 def getData(path):
     data = []
     datatruth = []
     for filename in glob.glob(path + "*.png"):
-        data.append(wordDetection(filename))
+        data.append(np.hstack([ShapeFeature(filename), wordDetection(filename)]))
     data = np.vstack(data)
     f = open(path + "tags.json")
     f = json.load(f)
@@ -19,13 +20,36 @@ def getData(path):
     datatruth = np.hstack(datatruth)
     return data, datatruth
 
-train, traintruth = getData("C:/Users/Alen/Documents/Machine Learning/BigData/Out/")
-test, testtruth = getData("C:/Users/Alen/Documents/Machine Learning/Small Data/Out/")
-print(train.shape)
-print(traintruth.shape)
-print(test.shape)
-print(testtruth.shape)
+train1, traintruth1 = getData("BigData1/Out/")
+train2, traintruth2 = getData("BigData2/Out/")
+train3, traintruth3 = getData("BigData3/Out/")
+test, testtruth = getData("SmallData/Out/")
+print("Train 1: ", train1.shape)
+print("Train truth 1: ", traintruth1.shape)
+print("Train 2: ", train2.shape)
+print("Train truth 2: ", traintruth2.shape)
+print("Train 3: ", train3.shape)
+print("Train truth 3: ", traintruth3.shape)
+print("Test: ", test.shape)
+print("Test truth: ", testtruth.shape)
 
+train = np.concatenate((train1, train2, train3), axis=0)
+traintruth = np.concatenate((traintruth1, traintruth2, traintruth3), axis=0)
+print("Train: ", train.shape)
+print("Train truth: ", traintruth.shape)
+
+""" # KNN
 classifier = neighbors.KNeighborsClassifier(10, weights='distance')
 classifier.fit(train, traintruth)
+guess = classifier.predict(test)
+mat = confusion_matrix(testtruth, geuss)
 print(classifier.score(test, testtruth))
+print(mat)
+"""
+# RF
+forest = RandomForestClassifier(criterion='entropy', n_estimators=100, n_jobs=-1, max_features = 10, max_depth = 20)
+forest.fit(train, traintruth)
+guess = forest.predict(test)
+mat = confusion_matrix(testtruth, guess)
+print(forest.score(test, testtruth))
+print(mat)
