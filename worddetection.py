e88df06e564a5bfbb720b14fbf35a68ad8395e7c
thomastymecki <77650237+thomastymecki@users.noreply.github.com>
2021-04-18 19:47:46 -0400

    Update worddetection.py

--- worddetection.py
+++ worddetection.py
@@ -19,15 +19,12 @@ def wordDetection(path):
     if any("STOP" in s for s in boo) or any("(STOP)" in s for s in boo) or any("STOPI" in s for s in boo) or any("STOP " in s for s in boo):
         if any("BUS" in s for s in boo):
             wordfeature.append(0)
-            print('bus')
         elif any("HERE" in s for s in boo) or any("TO" in s for s in boo):
             wordfeature.append(2*6/10)
         else:
             wordfeature.append(2*2/10)
-            print('stop')
     elif any("YIELD" in s for s in boo) or any("Yield" in s for s in boo) or any("YIELD/" in s for s in boo) or any("YIELD" in s for s in boo) or any("YIELD," in s for s in boo) or any("YIELD " in s for s in boo):
         wordfeature.append(2*3/10)
-        print('yield')
     elif any("NO" in s for s in boo) or any("NO PARKING" in s for s in boo) or any("GRASS" in s for s in boo) or any("Nu" in s for s in boo):
         wordfeature.append(0)
     elif any("TABLE" in s for s in boo):
@@ -38,7 +35,6 @@ def wordDetection(path):
         wordfeature.append(0)
     elif any("SPEED" in s for s in boo) or any("LIMIT" in s for s in boo) or any("15" in s for s in boo) or any("20" in s for s in boo) or any("10" in s for s in boo) or any("25" in s for s in boo) or any("SPEED LIMIT" in s for s in boo) or any("35" in s for s in boo) or any("45" in s for s in boo) or any("LIMT" in s for s in boo) or any("Limit" in s for s in boo)  or any("50" in s for s in boo) or any("MPH" in s for s in boo) or any("M.P.H." in s for s in boo) or any("22" in s for s in boo) or any("40" in s for s in boo):
         wordfeature.append(2*4/10)
-        print('speed')
     elif any("ONE WAY" in s for s in boo) or any("ONE" in s for s in boo) or any("WAY" in s for s in boo):
         wordfeature.append(0)
     elif any("TOW AWAY" in s for s in boo) or any("ZONE" in s for s in boo):
@@ -125,10 +121,8 @@ def wordDetection(path):
         wordfeature.append(0)
     elif any("PARKING" in s for s in boo) or any("DISABLED" in s for s in boo) or any("PERMIT" in s for s in boo) or any("PARKING BY DISABLED" in s for s in boo) or any("PERMIT ONLY" in s for s in boo) or any("PARKING BY" in s for s in boo) or any("PARKING BY DISABLED" in s for s in boo) or any("PARKING " in s for s in boo) or any("TOW-AWAY ZONE" in s for s in boo) or any("MAX FINE" in s for s in boo)  or any("HANDICAPPED" in s for s in boo) or any("FINE" in s for s in boo) or any("VAN" in s for s in boo) or any("PARKING |" in s for s in boo) or any("S250 MAX" in s for s in boo):
         wordfeature.append(2*5/10)
-        print('disabled')
     elif any("VEHICLES MUST" in s for s in boo) or any("AHFAD" in s for s in boo) or any("AHEAD" in s for s in boo) or any("ALCT" in s for s in boo) or any("KHEAD" in s for s in boo) or any("3" in s for s in boo) or any("TRAIL" in s for s in boo) or any("500" in s for s in boo) or any("FEET" in s for s in boo) or any("PEDESTRIAN" in s for s in boo):
         wordfeature.append(2*6/10)
-        print('pedestrian')
     elif any("ONLY" in s for s in boo):
         wordfeature.append(0)
     elif (len(boo)==0):
